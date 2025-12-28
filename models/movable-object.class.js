009b90c03b0bd0d4f52f9243ecb9bc125be946dc
Miruna Agopian <agopian.miruna@yahoo.com>
2025-12-28 18:38:38 +0100

    created rendering js logic for the main character and the chicken enemies

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,18 @@
 class MovableObject {
-    x;
-    y;
+    x = 80;
+    y = 300;
     img;
+    height = 150;
+    width = 80;
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
     moveRight(){
         console.log('object moving right');  
     }
     moveLeft(){
+        console.log('object moving left');
         
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,11 +1,28 @@
 class World {
+    context;
+    canvas;
     character = new Character();
     enemies = [
         new Chicken(), 
         new Chicken(), 
         new Chicken()
     ];
-    draw(){
 
+    constructor(canvas){
+        this.context = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
+
+    draw(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach(enemy => {
+            this.context.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
     }
 }
