b0c441195f215afc582866010f63f582c16bb2c5
Miruna Agopian <agopian.miruna@yahoo.com>
2026-02-04 20:02:42 +0100

    added JS Doc

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,22 +1,50 @@
+/**
+ * Base class for all drawable objects in the game.
+ * Provides position, size, and image‑loading functionality that other
+ * objects (characters, enemies, collectibles, UI elements) inherit.
+ */
 class DrawableObject {
-    x = 80;
-    y = 240;
-    height = 150;
-    width = 80;
-    img; 
-    imageCache = {};
+  /**
+   *  Default position and size of the drawable object.
+   *  These values can be overridden by subclasses.
+   */
+  x = 80;
+  y = 240;
+  height = 150;
+  width = 80;
 
-     loadImage(path){
-        this.img = new Image();
-        this.img.src = path;
-    }
+  /**
+   * The currently displayed image.
+   * @type {HTMLImageElement}
+   */
+  img;
 
-      loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+  /** 
+   * Cache storing preloaded images, keyed by their file paths. 
+   * Used to avoid reloading the same image multiple times.
+   */
+  imageCache = {};
 
+  /**
+   * Loads a single image and assigns it as the object's current sprite.
+   * @param {string} path - File path of the image to load.
+   */
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  /** 
+   * Preloads multiple images and stores them in the image cache. 
+   * Useful for animations or objects with multiple states. 
+   * 
+   * @param {string[]} arr - Array of image file paths to load. 
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
 }
--- models/endboss-bar.class.js
+++ models/endboss-bar.class.js
@@ -1,44 +1,64 @@
+/**
+ * Represents the status of the endbosse's energy level.
+ * It updates its state according to the number of hits the endboss gets.
+ * @extends DrawableObject
+ */
 class EndbossBar extends DrawableObject {
-    IMAGES_ENDBOSS_BAR_FULL = [
-        'img/7_statusbars/2_statusbar_endboss/green/green0.png',
-        'img/7_statusbars/2_statusbar_endboss/green/green20.png',
-        'img/7_statusbars/2_statusbar_endboss/green/green40.png',
-        'img/7_statusbars/2_statusbar_endboss/green/green60.png',
-        'img/7_statusbars/2_statusbar_endboss/green/green80.png',
-        'img/7_statusbars/2_statusbar_endboss/green/green100.png'
-    ];
+  /**
+   * Array of strings containing images paths for the health bar.
+   */
+  IMAGES_ENDBOSS_BAR_FULL = [
+    "img/7_statusbars/2_statusbar_endboss/green/green0.png",
+    "img/7_statusbars/2_statusbar_endboss/green/green20.png",
+    "img/7_statusbars/2_statusbar_endboss/green/green40.png",
+    "img/7_statusbars/2_statusbar_endboss/green/green60.png",
+    "img/7_statusbars/2_statusbar_endboss/green/green80.png",
+    "img/7_statusbars/2_statusbar_endboss/green/green100.png",
+  ];
 
-    constructor(){
-        super();
-        this.loadImages(this.IMAGES_ENDBOSS_BAR_FULL);
-        this.setPercentage(100);
-        this.x = 720 - this.width - 170;
-        this.y = 80;
-        this.width = 250;
-        this.height = 60;
-        this.visible = false;
-    }
+  /**
+   * Creates the status bar UI element and positions it on the canvas.
+   * It also initializes its default image (100% fill).
+   */
+  constructor() {
+    super();
+    this.loadImages(this.IMAGES_ENDBOSS_BAR_FULL);
+    this.setPercentage(100);
+    this.x = 720 - this.width - 170;
+    this.y = 80;
+    this.width = 250;
+    this.height = 60;
+    this.visible = false;
+  }
 
-    setPercentage(percentage){
+  /**
+   * Updates the bar's fill level by displaying the image corresponding to the percentage
+   * @param {number} percentage - the current health percentage of the status bar
+   */
+  setPercentage(percentage) {
     this.percentage = Math.min(percentage, 100);
     let images = this.IMAGES_ENDBOSS_BAR_FULL;
     let path = images[this.resolveImageIndex()];
     this.img = this.imageCache[path];
-   }
+  }
 
-    resolveImageIndex(){
-    if(this.percentage == 100) {
-        return 5;
-    } else if(this.percentage > 80){
-        return 4;
-    } else if(this.percentage > 60){
-        return 3;
-    } else if(this.percentage > 40){
-        return 2;
-    } else if(this.percentage > 20){
-        return 1;
+  /**
+   * Sets the image index corresponding to the percentage of the bar.
+   * @returns {number} - the index of the image to display from the array with image paths.
+   */
+  resolveImageIndex() {
+    if (this.percentage == 100) {
+      return 5;
+    } else if (this.percentage > 80) {
+      return 4;
+    } else if (this.percentage > 60) {
+      return 3;
+    } else if (this.percentage > 40) {
+      return 2;
+    } else if (this.percentage > 20) {
+      return 1;
     } else {
-        return 0;
+      return 0;
     }
-   }
+  }
 }
