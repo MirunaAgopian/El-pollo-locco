ced024dc900bc9c438bce22d914bce600edac97d
Miruna Agopian <agopian.miruna@yahoo.com>
2026-02-04 19:42:08 +0100

    added JSDoc

--- models/coin-bar.class.js
+++ models/coin-bar.class.js
@@ -1,4 +1,12 @@
+/**
+ * Represents the status of the collectible object 'coin'.
+ * It updates its state according to the number of collected coins.
+ * @extends DrawableObject
+ */
 class CoinBar extends DrawableObject {
+    /**
+     * Array of strings containing images paths for the coin bar [0% to 100%];
+     */
     IMAGES_COIN = [
     'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/0.png',
     'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/20.png',
@@ -8,6 +16,10 @@ class CoinBar extends DrawableObject {
     'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/100.png'
    ];
 
+   /**
+    * Creates the bottle bar UI element and positions it on the canvas.
+    * It also initializes its default image (0% fill).
+    */
    constructor(){
     super();
     this.loadImages(this.IMAGES_COIN);
@@ -18,6 +30,10 @@ class CoinBar extends DrawableObject {
     this.height = 60;
    }
 
+   /**
+    * Updates the bar's fill level by displaying the image corresponding to the percentage.
+    * @param {number} percentage - the current coin percentage of the bar
+    */
    setPercentage(percentage){
     this.percentage = Math.min(percentage, 100);
     let images = this.IMAGES_COIN;
@@ -25,6 +41,10 @@ class CoinBar extends DrawableObject {
     this.img = this.imageCache[path];
    }
 
+   /**
+    * Sets the image index corresponding to the percentage of the bar.
+    * @returns {number} - the index of the image to display from the array with image paths.
+    */
    resolveImageIndex(){
     if(this.percentage == 100) {
         return 5;
