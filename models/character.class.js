530f633dcb35aeccda471ed5494bcc577235ee6f
Miruna Agopian <agopian.miruna@yahoo.com>
2026-02-12 19:23:24 +0100

    exported character's sprites in a separate object and reduced the line of codes from within character.class.js

--- models/character.class.js
+++ models/character.class.js
@@ -34,91 +34,15 @@ class Character extends MovableObject {
   maxBottle = 6;
 
   /**
-   * Image paths for the character's long idle animation.
-   *  @type {string[]}
+   * Animation frame sequences imported from the global character animation system.
+   * These arrays contain the sprite paths for each animation state.
    */
-  IMAGES_LONG_IDLE = [
-    "img/2_character_pepe/1_idle/long_idle/I-11.png",
-    "img/2_character_pepe/1_idle/long_idle/I-12.png",
-    "img/2_character_pepe/1_idle/long_idle/I-13.png",
-    "img/2_character_pepe/1_idle/long_idle/I-14.png",
-    "img/2_character_pepe/1_idle/long_idle/I-15.png",
-    "img/2_character_pepe/1_idle/long_idle/I-16.png",
-    "img/2_character_pepe/1_idle/long_idle/I-17.png",
-    "img/2_character_pepe/1_idle/long_idle/I-18.png",
-    "img/2_character_pepe/1_idle/long_idle/I-19.png",
-    "img/2_character_pepe/1_idle/long_idle/I-12.png",
-  ];
-
-  /**
-   * Image paths for the character's idle animation.
-   *  @type {string[]}
-   */
-  IMAGES_IDLE = [
-    "img/2_character_pepe/1_idle/idle/I-1.png",
-    "img/2_character_pepe/1_idle/idle/I-2.png",
-    "img/2_character_pepe/1_idle/idle/I-3.png",
-    "img/2_character_pepe/1_idle/idle/I-4.png",
-    "img/2_character_pepe/1_idle/idle/I-5.png",
-    "img/2_character_pepe/1_idle/idle/I-6.png",
-    "img/2_character_pepe/1_idle/idle/I-7.png",
-    "img/2_character_pepe/1_idle/idle/I-8.png",
-    "img/2_character_pepe/1_idle/idle/I-9.png",
-    "img/2_character_pepe/1_idle/idle/I-10.png",
-  ];
-
-  /**
-   * Image paths for the character's walking animation.
-   *  @type {string[]}
-   */
-  IMAGES_WALKING = [
-    "img/2_character_pepe/2_walk/W-21.png",
-    "img/2_character_pepe/2_walk/W-22.png",
-    "img/2_character_pepe/2_walk/W-23.png",
-    "img/2_character_pepe/2_walk/W-24.png",
-    "img/2_character_pepe/2_walk/W-25.png",
-    "img/2_character_pepe/2_walk/W-26.png",
-  ];
-
-  /**
-   * Image paths for the character's jump animation.
-   *  @type {string[]}
-   */
-  IMAGES_JUMPING = [
-    "img/2_character_pepe/3_jump/J-31.png",
-    "img/2_character_pepe/3_jump/J-32.png",
-    "img/2_character_pepe/3_jump/J-33.png",
-    "img/2_character_pepe/3_jump/J-34.png",
-    "img/2_character_pepe/3_jump/J-35.png",
-    "img/2_character_pepe/3_jump/J-36.png",
-    "img/2_character_pepe/3_jump/J-37.png",
-    "img/2_character_pepe/3_jump/J-38.png",
-    "img/2_character_pepe/3_jump/J-39.png",
-  ];
-
-  /**
-   * Image paths for the character's hurt animation.
-   *  @type {string[]}
-   */
-  IMAGES_HURT = [
-    "img/2_character_pepe/4_hurt/H-41.png",
-    "img/2_character_pepe/4_hurt/H-42.png",
-    "img/2_character_pepe/4_hurt/H-43.png",
-  ];
-
-  /**
-   * Image paths for the character's dead animation.
-   *  @type {string[]}
-   */
-  IMAGES_DEAD = [
-    "img/2_character_pepe/5_dead/D-51.png",
-    "img/2_character_pepe/5_dead/D-52.png",
-    "img/2_character_pepe/5_dead/D-53.png",
-    "img/2_character_pepe/5_dead/D-54.png",
-    "img/2_character_pepe/5_dead/D-55.png",
-    "img/2_character_pepe/5_dead/D-56.png",
-    "img/2_character_pepe/5_dead/D-57.png",
-  ];
+  IMAGES_LONG_IDLE = characterAnimationImages.IMAGES_LONG_IDLE;
+  IMAGES_IDLE = characterAnimationImages.IMAGES_IDLE;
+  IMAGES_WALKING = characterAnimationImages.IMAGES_WALKING;
+  IMAGES_JUMPING = characterAnimationImages.IMAGES_JUMPING;
+  IMAGES_HURT = characterAnimationImages.IMAGES_HURT;
+  IMAGES_DEAD = characterAnimationImages.IMAGES_DEAD;
 
   /**
    * Creates a new character instance, initializes animation states,
@@ -178,7 +102,6 @@ class Character extends MovableObject {
   }
 
   /**
-   * Handles horizontal character movement based on keyboard input.
    * Moves the character left or right if the corresponding key is pressed
    * and the character remains within the level boundaries.
    */
@@ -229,7 +152,6 @@ class Character extends MovableObject {
   }
 
   /**
-   * Handles the hurt animation state.
    * Plays the hurt animation if the character is damaged and resets idle-related timers.
    * @returns {boolean} True if the hurt animation was triggered, otherwise false.
    */
@@ -249,7 +171,6 @@ class Character extends MovableObject {
   }
 
   /**
-   * Handles the death animation state.
    * Plays the death animation and triggers the end screen once the animation finishes.
    * @returns {boolean} True if the death animation was triggered, otherwise false.
    */
@@ -281,7 +202,7 @@ class Character extends MovableObject {
     const lastFrame = this.IMAGES_DEAD.length - 1;
     if (this.currentImg === lastFrame && !this.deathAnimationPlayed) {
       this.deathAnimationPlayed = true;
-      setTimeout(() => endGame('lose'), 3000);
+      setTimeout(() => endGame("lose"), 3000);
     }
   }
 
---
+++ systems/character-animation-system.js
@@ -0,0 +1,96 @@
+/**
+ * Global object containing all sprite image paths used for the character's animations.
+ * 
+ * This object groups the image sequences for each animation state
+ * (idle, long idle, walking, jumping, hurt, dead) and makes them
+ * accessible to the Character class without requiring imports.
+ * 
+ * Each property is an array of file paths representing the frames
+ * of a specific animation.
+ * 
+ * @type {Object<string, string[]>}
+ */
+
+window.characterAnimationImages = { 
+  /**
+   * Image paths for the character's long idle animation.
+   *  @type {string[]}
+   */
+  IMAGES_LONG_IDLE: [
+    "img/2_character_pepe/1_idle/long_idle/I-11.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12.png",
+    "img/2_character_pepe/1_idle/long_idle/I-13.png",
+    "img/2_character_pepe/1_idle/long_idle/I-14.png",
+    "img/2_character_pepe/1_idle/long_idle/I-15.png",
+    "img/2_character_pepe/1_idle/long_idle/I-16.png",
+    "img/2_character_pepe/1_idle/long_idle/I-17.png",
+    "img/2_character_pepe/1_idle/long_idle/I-18.png",
+    "img/2_character_pepe/1_idle/long_idle/I-19.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12.png",
+  ],
+  /**
+   * Image paths for the character's idle animation.
+   *  @type {string[]}
+   */
+  IMAGES_IDLE: [
+    "img/2_character_pepe/1_idle/idle/I-1.png",
+    "img/2_character_pepe/1_idle/idle/I-2.png",
+    "img/2_character_pepe/1_idle/idle/I-3.png",
+    "img/2_character_pepe/1_idle/idle/I-4.png",
+    "img/2_character_pepe/1_idle/idle/I-5.png",
+    "img/2_character_pepe/1_idle/idle/I-6.png",
+    "img/2_character_pepe/1_idle/idle/I-7.png",
+    "img/2_character_pepe/1_idle/idle/I-8.png",
+    "img/2_character_pepe/1_idle/idle/I-9.png",
+    "img/2_character_pepe/1_idle/idle/I-10.png",
+  ],
+  /**
+   * Image paths for the character's walking animation.
+   *  @type {string[]}
+   */
+  IMAGES_WALKING: [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ],
+  /**
+   * Image paths for the character's jump animation.
+   *  @type {string[]}
+   */
+  IMAGES_JUMPING: [
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+  ],
+  /**
+   * Image paths for the character's hurt animation.
+   *  @type {string[]}
+   */
+  IMAGES_HURT: [
+    "img/2_character_pepe/4_hurt/H-41.png",
+    "img/2_character_pepe/4_hurt/H-42.png",
+    "img/2_character_pepe/4_hurt/H-43.png",
+  ],
+  /**
+   * Image paths for the character's dead animation.
+   *  @type {string[]}
+   */
+  IMAGES_DEAD: [
+    "img/2_character_pepe/5_dead/D-51.png",
+    "img/2_character_pepe/5_dead/D-52.png",
+    "img/2_character_pepe/5_dead/D-53.png",
+    "img/2_character_pepe/5_dead/D-54.png",
+    "img/2_character_pepe/5_dead/D-55.png",
+    "img/2_character_pepe/5_dead/D-56.png",
+    "img/2_character_pepe/5_dead/D-57.png",
+  ],
+};
